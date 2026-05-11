9f85a118b82d8b1be505bd261276f6ed044726cc
Daniel-Aderinola <kayodeaderinola0@gmail.com>
2026-05-11 11:17:26 +0100

    fixes

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,4 +22,13 @@ import bookingRoutes from './routes/bookingRoutes'
 app.use('/api/cv', cvRoutes)
 app.use('/api/bookings', bookingRoutes)
 
+// Serve static files from client build
+const clientBuildPath = path.join(__dirname, '..', '..', 'client', 'dist')
+if (fs.existsSync(clientBuildPath)) {
+  app.use(express.static(clientBuildPath))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(clientBuildPath, 'index.html'))
+  })
+}
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
