1394e6383919d2b526044b2dd08c4abdf8a67fb2
Daniel-Aderinola <kayodeaderinola0@gmail.com>
2026-05-11 10:56:26 +0100

    fixes

--- server/src/routes/cvRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import upload from '../middleware/uploadMiddleware'
-import { submitCV } from '../controllers/cvController'
-
-const router = Router()
-
-router.post('/', upload.single('cv'), submitCV)
-
-export default router
